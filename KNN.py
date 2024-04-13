4dce29a8426af056f4178a93a4742d908d5818a9
jiashurui <jiashurui1997@gmail.com>
2024-04-13 18:23:12 +0900

    start

---
+++ KNN.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+np.random.random
