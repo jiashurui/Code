fd612f991018bacc052ef48ee0d2fa0fc8bb8046
jiashurui <jiashurui1997@gmail.com>
2024-04-13 22:31:13 +0900

    knn

--- supervised_learning/KNN.py
+++ supervised_learning/KNN.py
@@ -21,85 +21,49 @@ X1, Y1 = make_classification(
 # 定义颜色映射
 colors = plt.cm.tab10
 
-plot = plt.scatter(X1[:, 0], X1[:, 1], marker="o", c=Y1,cmap=colors, s=10, )
+plot = plt.scatter(X1[:, 0], X1[:, 1], marker="o", c=Y1, cmap=colors, s=10, )
 
-data = np.array([0.,0.])
+data = np.array([0., 0.])
 
 # plt.show()
-distance_array = np.empty([100,3])
+distance_array = np.empty([100, 3])
 
 for i in range(count):
     # pairwise_distances(X1[i], data, metric='euclidean', n_jobs=-1)
     distance = np.linalg.norm(data - X1[i])
-    distance_array[i,0] = X1[i][0]
-    distance_array[i,1] = X1[i][1]
-    distance_array[i,2] = distance
+    distance_array[i, 0] = X1[i][0]
+    distance_array[i, 1] = X1[i][1]
+    distance_array[i, 2] = distance
 
-
-min_indices = np.argsort(distance_array[:,2])[:K]
-
+min_indices = np.argsort(distance_array[:, 2])[:K]
 
 # connect
-print(distance_array[min_indices][4][0:2])
-
-
-
-
 count_true = np.int8(0)
 count_false = np.int8(0)
 
 for index in min_indices:
-    if Y1[index]:
-        count_true+=1
+    if Y1[index] == 1:
+        count_true += 1
     else:
-        count_false+=1
+        count_false += 1
 
-result = np.nan
+result = -1
 if count_true > count_false:
     result = np.array([3])
 else:
-    result = np.array([3])
+    result = np.array([4])
 print(result)
-plt.scatter(data[0], data[1], c=result, marker='o',cmap=colors, s=20, )
+plt.scatter(data[0], data[1], c=result, marker='o', cmap=colors, s=20, )
 
-print(distance_array[min_indices][0])
-print(distance_array[min_indices][0][0:2])
-plt.plot([data[0],distance_array[min_indices][0][0]] ,[data[1],distance_array[min_indices][0][1]] ,color='red',linestyle='solid')
-plt.plot([data[0],distance_array[min_indices][1][0]] ,[data[1],distance_array[min_indices][1][1]] ,color='red',linestyle='solid')
-plt.plot([data[0],distance_array[min_indices][2][0]] ,[data[1],distance_array[min_indices][2][1]] ,color='red',linestyle='solid')
-plt.plot([data[0],distance_array[min_indices][3][0]] ,[data[1],distance_array[min_indices][3][1]] ,color='red',linestyle='solid')
-plt.plot([data[0],distance_array[min_indices][4][0]] ,[data[1],distance_array[min_indices][4][1]] ,color='red',linestyle='solid')
-
+plt.plot([data[0], distance_array[min_indices][0][0]], [data[1], distance_array[min_indices][0][1]], color='red',
+         linestyle='solid')
+plt.plot([data[0], distance_array[min_indices][1][0]], [data[1], distance_array[min_indices][1][1]], color='red',
+         linestyle='solid')
+plt.plot([data[0], distance_array[min_indices][2][0]], [data[1], distance_array[min_indices][2][1]], color='red',
+         linestyle='solid')
+plt.plot([data[0], distance_array[min_indices][3][0]], [data[1], distance_array[min_indices][3][1]], color='red',
+         linestyle='solid')
+plt.plot([data[0], distance_array[min_indices][4][0]], [data[1], distance_array[min_indices][4][1]], color='red',
+         linestyle='solid')
 
 plt.show()
-
-# plt.subplot(322)
-
-# plt.title("Two informative features, one cluster per class", fontsize="small")
-# X1, Y1 = make_classification(
-#     n_features=2, n_redundant=0, n_informative=2, n_clusters_per_class=1
-# )
-# plt.scatter(X1[:, 0], X1[:, 1], marker="o", c=Y1, s=25, edgecolor="k")
-#
-# plt.subplot(323)
-# plt.title("Two informative features, two clusters per class", fontsize="small")
-# X2, Y2 = make_classification(n_features=2, n_redundant=0, n_informative=2)
-# plt.scatter(X2[:, 0], X2[:, 1], marker="o", c=Y2, s=25, edgecolor="k")
-#
-# plt.subplot(324)
-# plt.title("Multi-class, two informative features, one cluster", fontsize="small")
-# X1, Y1 = make_classification(
-#     n_features=2, n_redundant=0, n_informative=2, n_clusters_per_class=1, n_classes=3
-# )
-# plt.scatter(X1[:, 0], X1[:, 1], marker="o", c=Y1, s=25, edgecolor="k")
-#
-# plt.subplot(325)
-# plt.title("Three blobs", fontsize="small")
-# X1, Y1 = make_blobs(n_features=2, centers=3)
-# plt.scatter(X1[:, 0], X1[:, 1], marker="o", c=Y1, s=25, edgecolor="k")
-#
-# plt.subplot(326)
-# plt.title("Gaussian divided into three quantiles", fontsize="small")
-# X1, Y1 = make_gaussian_quantiles(n_features=2, n_classes=3)
-# plt.scatter(X1[:, 0], X1[:, 1], marker="o", c=Y1, s=25, edgecolor="k")
-
