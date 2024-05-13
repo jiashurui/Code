e30a7d8ea8e4e0c32e6b0e8c0229970ff28b3af9
jiashurui <jiashurui1997@gmail.com>
2024-05-13 20:58:41 +0900

    1123

--- prototype/1dcnn.py
+++ prototype/1dcnn.py
@@ -14,15 +14,17 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 示例数据
 channel = 1  # 输入通道数
-slide_window_length = 100  # 序列长度
+slide_window_length = 200  # 序列长度
 stripe = int(slide_window_length * 0.5)  # overlap 50%
-epochs = 20
-batch_size = 64  # 或其他合适的批次大小
-stop_simple = 300  # 数据静止的个数
-learning_rate = 0.005
+epochs = 100
+batch_size = 128  # 或其他合适的批次大小
+stop_simple = 500  # 数据静止的个数
+learning_rate = 0.0001
 # 创建示例输入数据 TODO
 file_list = glob.glob('../data/realworld/*/acc_walking_*.csv')
 final_data = []
+
+
 
 # make label by fileName (walking)
 # chest 1 forearm 2 head 3 shin 4 thigh 5 upper arm 6 waist 7
@@ -100,6 +102,8 @@ model.train()
 lost_arr = []
 
 for epoch in range(epochs):
+    # 毎回異なるデータ抽出
+    random.shuffle(final_data)
     permutation = torch.randperm(train_data.size()[0])
     for i in range(0, train_data.size()[0], batch_size):
         optimizer.zero_grad()
@@ -128,21 +132,27 @@ torch.save(model.state_dict(), '../model/1D-CNN.pth')
 ################################################################################
 ################################################################################
 # 实例化模型(加载模型参数)
-model_load = Simple1DCNN()
+model_load = Simple1DCNN().to(device)
 model_load.load_state_dict(torch.load('../model/1D-CNN.pth'))
 
 model_load.eval()
-test_loss = 0
+num_sum = 0
 correct = 0
+test_loss = 0
 with torch.no_grad():
 
     for i in range(0, test_data.size()[0], batch_size):
         input_data, label = train_data[i: i + batch_size], test_labels[i: i + batch_size]
-        outputs = model_load(input_data)
-        # loss += loss_function(outputs, label).item()
-        pred = outputs.argmax(dim=1, keepdim=True)  # 获取概率最大的索引
-        print()
+        if label.size(0) != batch_size:
+            continue
 
-test_loss /= len(test_data)
+        outputs = model_load(input_data)
+
+        # test_loss += loss_function(outputs, label).item()
+        pred = outputs.argmax(dim=1, keepdim=True)  # 获取概率最大的索引
+
+        correct += torch.eq(pred, label.reshape(batch_size,1)).sum().item()
+        num_sum += batch_size
+
 print(
-    f'\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_data)} ({100. * correct / len(test_data):.0f}%)\n')
+    f'\nTest set: Average loss: {test_loss/num_sum:.4f}, Accuracy: {correct}/{num_sum} ({100. * correct / num_sum:.0f}%)\n')
