50ff205bd9a03842b41e8f16964aa752275c32a8
jiashurui <jiashurui1997@gmail.com>
2024-10-16 15:36:41 +0900

    OC SVM

--- anormal/one_class_svm.py
+++ anormal/one_class_svm.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 from sklearn import svm
+from sklearn.metrics import confusion_matrix
 
 from utils.uci_datareader import get_data_1d_uci_all_data
 
@@ -14,8 +17,6 @@ train_abnormal = train_abnormal.numpy().reshape(-1, train_abnormal.shape[2])
 test_normal = test_normal.numpy().reshape(-1, test_normal.shape[2])
 test_abnormal = test_abnormal.numpy().reshape(-1, test_abnormal.shape[2])
 
-
-
 # 训练 One-Class SVM 模型
 model = svm.OneClassSVM(kernel='rbf', gamma=0.1, nu=0.1)
 model.fit(train_normal)
@@ -27,18 +28,31 @@ test_normal_pred = model.predict(test_normal)
 test_abnormal_pred = model.predict(test_abnormal)
 
 
-print()
-# 可视化结果 (仅限前两个维度)
-# plt.title("One-Class SVM for Anomaly Detection")
-# plt.scatter(x_train[:, 0], x_train[:, 1], c='white', s=20, edgecolor='k', label="Training Data")
-# plt.scatter(x_test[:, 0], x_test[:, 1], c='blue', s=20, edgecolor='k', label="Test Data")
-# plt.scatter(x_outliers[:, 0], x_outliers[:, 1], c='red', s=20, edgecolor='k', label="Outliers")
-#
-# # 绘制决策边界 (仅限前两个维度)
-# xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-5, 5, 500))
-# z = model.decision_function(np.c_[xx.ravel(), yy.ravel(), np.zeros((xx.ravel().shape[0], 8))])
-# z = z.reshape(xx.shape)
-# plt.contour(xx, yy, z, levels=[0], linewidths=2, colors='black')
-#
-# plt.legend()
-# plt.show()
+# 统计结果 训练集
+train_tp = np.sum(train_normal_pred == 1)
+train_tn = np.sum(train_abnormal_pred == -1)
+
+train_fp = np.sum(train_abnormal_pred == 1)
+train_fn = np.sum(train_normal_pred == -1)
+
+# 统计结果 测试集
+test_tp = np.sum(test_normal_pred == 1)
+test_tn = np.sum(test_abnormal_pred == -1)
+
+test_fp = np.sum(test_abnormal_pred == 1)
+test_fn = np.sum(test_normal_pred == -1)
+
+# 合并测试集的预测结果和真实标签
+y_true = np.concatenate([np.ones(len(test_normal)), -np.ones(len(test_abnormal))])
+y_pred = np.concatenate([test_normal_pred, test_abnormal_pred])
+
+# 计算混淆矩阵
+cm = confusion_matrix(y_true, y_pred, labels=[1, -1])
+
+# 可视化混淆矩阵
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Normal', 'Abnormal'], yticklabels=['Normal', 'Abnormal'])
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.show()
