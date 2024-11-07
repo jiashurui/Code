e4f7326ba78952349d5f3fb146668856c7220981
jiashurui <jiashurui1997@gmail.com>
2024-11-07 22:30:39 +0900

    lstm-GAN

--- gan/lstm_gan.py
+++ gan/lstm_gan.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 class Generator(nn.Module):
     def __init__(self, z_dim, hidden_dim, output_dim):
         super(Generator, self).__init__()
-        self.lstm = nn.LSTM(z_dim, hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(z_dim, hidden_dim, num_layers=3, batch_first=True, dropout=0.1)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, z):
@@ -19,11 +19,14 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(Discriminator, self).__init__()
-        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=3, batch_first=True, dropout=0.1)
         self.fc = nn.Linear(hidden_dim, 1)
 
     def forward(self, x):
         # x 的维度: (batch_size, seq_len, input_dim)
+
+        # 增加噪声
+        x = x + torch.normal(0, 0.1, size=x.size()).to(x.device)
         lstm_out, _ = self.lstm(x)
         # 取最后一个时间步的输出来判断真假
         out = self.fc(lstm_out[:, -1, :])  # 输出维度: (batch_size, 1)
