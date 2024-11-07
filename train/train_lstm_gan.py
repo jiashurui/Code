de4471d62af6d07bddc37e1e58f0ea659fc85a24
jiashurui <jiashurui1997@gmail.com>
2024-11-07 21:39:00 +0900

    lstm-GAN

--- train/train_lstm_gan.py
+++ train/train_lstm_gan.py
@@ -15,7 +15,7 @@ epochs = 1000
 batch_size = 64
 
 slice_length = 256
-filtered_label = [0, 2, 3, 5, 6, 7, 8, 9, 10, 12]
+filtered_label = [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12]
 mapping = constant.Constant.simple_action_set.mapping_mh
 
 # 全局变换之后的大学生数据(全局变换按照frame进行)
