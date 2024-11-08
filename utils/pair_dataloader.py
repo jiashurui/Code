58a8b5f5fb83b9f9b4ec5edd7f1aec0e2ccd6b1d
jiashurui <jiashurui1997@gmail.com>
2024-11-08 14:36:36 +0900

    random

--- utils/pair_dataloader.py
+++ utils/pair_dataloader.py
@@ -1,4 +1,6 @@
 # 定义自定义的 Dataset 类，用于加载源域和目标域的数据对
+import random
+
 from torch.utils.data import Dataset
 
 
@@ -15,8 +17,9 @@ class PairedDataset(Dataset):
         return self.length
 
     def __getitem__(self, idx):
-        source_idx = idx % self.source_len  # 防止索引超出范围
-        target_idx = idx % self.target_len  # 防止索引超出范围
+        # 随机选择源域和目标域的样本
+        source_idx = random.randint(0, self.source_len - 1)
+        target_idx = random.randint(0, self.target_len - 1)
         source_sample = self.source_data[source_idx]
         target_sample = self.target_data[target_idx]
         return source_sample, target_sample
